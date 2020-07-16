462bb7bc77a6dcf6b2861772a927b097c44ff908
Salim B <salim@posteo.de>
2020-07-17 01:04:57 +0200

    overhaul code

--- R/tocr.R
+++
@@ -1,7 +0,0 @@
-#' @details
-#' - mention most important functions (`add_toc()` and `remove_toc()`)
-#' - mention vignettes...`intro.Rmd`
-#' - inspired by...
-#' - alternatives...
-#' @import magrittr
-"_PACKAGE"
